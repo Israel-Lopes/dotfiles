621cf081e5c9d5ba27f39d172617a7f543844645
Israel-Lopes <yisrael.lopes@gmail.com>
2023-05-01 22:13:57 -0300

    adcionando less

--- git/ghistory.sh
+++ git/ghistory.sh
@@ -3,4 +3,4 @@
 # Obtém o histórico do git com informações sobre as branchs e formatação personalizada
 git log --graph --abbrev-commit --decorate --format=format:'%C(auto)%h%d %s %C(black)%C(bold)%cr' --color |
 # Substitui os caracteres ASCII usados para o fluxo das branchs pelos equivalentes em Unicode
-sed -e 's/|/│/g' -e 's/\\-/--/g' -e 's/*/●/g' -e 's/\\/╲/g' -e 's/_/─/g' -e 's/-/─/g' -e 's/>/▶/g' -e 's/</◀/g'
+sed -e 's/|/│/g' -e 's/\\-/--/g' -e 's/*/●/g' -e 's/\\/╲/g' -e 's/_/─/g' -e 's/-/─/g' -e 's/>/▶/g' -e 's/</◀/g' | less
